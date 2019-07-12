320b656f1d03ba9aa4c827d1e329f5725ec0ec06
michaelbASC6 <mikebunch28@gmail.com>
2019-07-12 12:40:01 -0400

    Mash

---
+++ Mash.js
@@ -0,0 +1,24 @@
+function someFunction(){
+return GetChildrenCount();
+}
+console.log(someFunction());
+
+//Version 2//
+function getHome(){
+let getHome = ["Mansion", "Apartment", "Shack", "House"];
+let H = Math.floor(Math.random()*getHome.length)
+return getHome[H]
+
+}
+//Version3//
+function GetChildrenCount(){
+ 
+    
+
+
+   let i = Math.floor(Math.random()*100)
+    Math.floor(Math.random()*100)
+    return i
+    }
+//Version4//
+
